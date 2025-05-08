eab4a772f80cca68ccb268c9c5b03d4321e72898
Mika Senghaas <mail@mikasenghaas.de>
2025-05-08 01:31:04 +0000

    Switch to using pickled pre-computed rewards for unit tests

--- tests/units/test_rewards.py
+++ tests/units/test_rewards.py
@@ -1,122 +1,44 @@
-import json
+import pickle
 import pytest
-from collections import defaultdict
-
-from vllm import RequestOutput, CompletionOutput
 
 from zeroband.inference.rewards import compute_rewards
 
 
 @pytest.fixture
-def samples():
-    with open("tests/units/example_rewards.json", "r") as f:
-        return json.load(f)
+def precomputed_rewards(path):
+    with open(path, "rb") as f:
+        return pickle.load(f)
 
 
-@pytest.fixture
-def request_outputs(samples):
-    request_outputs_dict = defaultdict(dict)
-    for sample in samples["samples"]:
-        request_id = sample["request_idx"]
-        completion_id = sample["output_idx"]
-        request_outputs_dict[request_id][completion_id] = CompletionOutput(
-            index=completion_id,
-            token_ids=None,
-            text=sample["completion"],
-            cumulative_logprob=None,
-            logprobs=None,
-        )
+@pytest.mark.parametrize("path", ["tests/units/rewards.pkl"])
+def test_compute_rewards(precomputed_rewards):
+    # Get inputs and outputs
+    request_outputs = precomputed_rewards["request_outputs"]
+    verification_infos = precomputed_rewards["verification_infos"]
+    task_types = precomputed_rewards["task_types"]
+    config = precomputed_rewards["config"]
+    ground_truth_rewards = precomputed_rewards["rewards"]
+    ground_truth_task_rewards = precomputed_rewards["task_rewards"]
+    ground_truth_length_penalties = precomputed_rewards["length_penalties"]
+    ground_truth_advantages = precomputed_rewards["advantages"]
 
-    request_outputs = []
-    for request_id, completion_outputs in request_outputs_dict.items():
-        completion_outputs = list(completion_outputs.values())
-        request_outputs.append(
-            RequestOutput(
-                request_id=request_id,
-                prompt=None,
-                prompt_token_ids=None,
-                prompt_logprobs=None,
-                outputs=completion_outputs,
-                finished=True,
-            )
-        )
-
-    yield request_outputs
-
-
-@pytest.fixture
-def verification_infos(samples):
-    verification_infos = []
-    request_ids = set()
-    for sample in samples["samples"]:
-        request_id = sample["request_idx"]
-        if request_id not in request_ids:
-            verification_infos.append(json.loads(sample["verification_info"]))
-            request_ids.add(request_id)
-    return verification_infos
-
-
-@pytest.fixture
-def ground_truth_rewards(samples):
-    rewards = defaultdict(list)
-    for sample in samples["samples"]:
-        request_id = sample["request_idx"]
-        rewards[request_id].append(sample["reward"])
-
-    yield rewards
-
-
-@pytest.fixture
-def ground_truth_task_rewards(samples):
-    task_rewards = defaultdict(list)
-    for sample in samples["samples"]:
-        request_id = sample["request_idx"]
-        task_rewards[request_id].append(sample["task_reward"])
-
-    yield task_rewards
-
-
-@pytest.fixture
-def ground_truth_length_penalties(samples):
-    penalties = defaultdict(list)
-    for sample in samples["samples"]:
-        request_id = sample["request_idx"]
-        penalties[request_id].append(sample["length_penalty"])
-    yield penalties
-
-
-@pytest.fixture
-def ground_truth_advantages(samples):
-    advantages = defaultdict(dict)
-    for sample in samples["samples"]:
-        request_id = sample["request_idx"]
-        completion_id = sample["output_idx"]
-        advantages[request_id][completion_id] = sample["advantage"]
-    advantages = {k: list(v.values()) for k, v in advantages.items()}
-    yield advantages
-
-
-def test_compute_rewards(
-    request_outputs,
-    ground_truth_rewards,
-    ground_truth_task_rewards,
-    ground_truth_length_penalties,
-    ground_truth_advantages,
-    verification_infos,
-):
     # Re-compute rewards
     task_types = ["verifiable_math"] * len(request_outputs)
     rewards, task_rewards, length_penalties, advantages = compute_rewards(
         request_outputs,
         verification_infos,
         task_types,
-        config=None,
+        config=config,
     )
+    print("rewards", rewards)
+    print("task_rewards", task_rewards)
+    print("length_penalties", length_penalties)
+    print("advantages", advantages)
 
     # Assert type
     for reward_or_advantage in [rewards, task_rewards, length_penalties, advantages]:
         assert isinstance(reward_or_advantage, dict)
-        assert all(isinstance(key, int) for key in reward_or_advantage.keys())
+        assert all(isinstance(key, str) for key in reward_or_advantage.keys())
         assert all(isinstance(value, list) for value in reward_or_advantage.values())
 
     # Assert computation
