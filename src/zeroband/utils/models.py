0d64219b71745aedc2ce2c83b7bb6cb1bfe2e736
Justus Mattern <122226645+justusmattern27@users.noreply.github.com>
2025-05-23 02:16:53 +0200

    remove train logger from model registry

--- src/zeroband/utils/models.py
+++ src/zeroband/utils/models.py
@@ -2,11 +2,6 @@ from typing import Literal, TypeAlias
 
 from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer, LlamaForCausalLM, Qwen2ForCausalLM, Qwen3ForCausalLM
 
-from zeroband.utils.logger import get_logger
-
-# Global logger
-logger = get_logger("TRAIN")
-
 
 ModelName: TypeAlias = Literal[
     # Llama 3
