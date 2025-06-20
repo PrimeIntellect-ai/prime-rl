346eb1ee419b2223381248d3ede3f0749b189e8f
Mika Senghaas <mail@mikasenghaas.de>
2025-06-20 06:28:46 +0000

    Fixing prompts test

--- src/zeroband/infer.py
+++ src/zeroband/infer.py
@@ -264,6 +264,7 @@ def inference(config: InferenceConfig):
             config.rewards.len_reward,
             tokenizer=tokenizer,
             enable_thinking=config.model.enable_thinking,
+            tokenize=True,
         )
 
         # Convert BatchEncoding to TokensPrompt objects
