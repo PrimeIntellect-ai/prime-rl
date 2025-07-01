46772e8bb517558f2fd80f620750dcd15ad508c8
Mika Senghaas <mail@mikasenghaas.de>
2025-07-01 01:50:10 +0000

    Remove unused code

--- src/zeroband/training/utils.py
+++ src/zeroband/training/utils.py
@@ -1,4 +1,3 @@
-import socket
 from itertools import chain
 from typing import TypeAlias
 
@@ -19,25 +18,6 @@ def seed_everything(seed: int):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
-
-
-def get_random_available_port_list(num_port):
-    # https://stackoverflow.com/questions/1365265/on-localhost-how-do-i-pick-a-free-port-number
-    ports = []
-
-    while len(ports) < num_port:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind(("", 0))
-            new_port = s.getsockname()[1]
-
-        if new_port not in ports:
-            ports.append(new_port)
-
-    return ports
-
-
-def get_random_available_port():
-    return get_random_available_port_list(1)[0]
 
 
 class FakeTokenizer(object):
