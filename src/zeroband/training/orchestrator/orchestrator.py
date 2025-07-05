07554e8937303a99748d51bfad26e54ed090b356
Mika Senghaas <mail@mikasenghaas.de>
2025-07-05 16:50:18 +0000

    Rename key

--- src/zeroband/training/orchestrator/orchestrator.py
+++ src/zeroband/training/orchestrator/orchestrator.py
@@ -128,7 +128,7 @@ async def orchestrate(config: OrchestratorConfig, setup_queue: Queue | None = No
 
         # Optionally, wait for the next checkpoint to be available
         async_level = step - 1 - ckpt_step  # How many steps training ahead
-        wait_for_ckpt_time, reload_weights_time = 0, 0
+        wait_for_weight_ckpt_time, reload_weights_time = 0, 0
         if async_level > config.async_level:
             ckpt_step = step - 1 - config.async_level
             logger.debug(
@@ -136,12 +136,14 @@ async def orchestrate(config: OrchestratorConfig, setup_queue: Queue | None = No
             )
 
             # Wait for the checkpoint to be available
-            wait_for_ckpt_start_time = time.time()
+            logger.debug(f"Waiting for weight checkpoint for step {ckpt_step}")
+            wait_for_weight_ckpt_start_time = time.time()
             wait_for_weight_checkpoint(config.weights.path, ckpt_step)
-            wait_for_ckpt_time = time.time() - wait_for_ckpt_start_time
-            logger.debug(f"Waited {wait_for_ckpt_time:.2f}s for checkpoint")
+            wait_for_weight_ckpt_time = time.time() - wait_for_weight_ckpt_start_time
+            logger.debug(f"Waited {wait_for_weight_ckpt_time:.2f}s for weight checkpoint")
 
             # Reload the weights
+            logger.debug(f"Reloading weights for step {ckpt_step}")
             reload_weights_start_time = time.time()
             await reload_weights(client, config.weights.path, ckpt_step)
             reload_weights_time = time.time() - reload_weights_start_time
@@ -275,7 +277,7 @@ async def orchestrate(config: OrchestratorConfig, setup_queue: Queue | None = No
         # Log time metrics to monitor
         time_metrics = {
             "time/infer": step_time,
-            "time/infer/wait_for_ckpt": wait_for_ckpt_time,
+            "time/infer/wait_for_weight_ckpt": wait_for_weight_ckpt_time,
             "time/infer/generate_completions": generate_completions_time,
             "time/infer/compute_rewards": compute_rewards_time,
             "time/infer/reload_weights": reload_weights_time,
