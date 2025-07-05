7ceca4c1f7ed26d9ae7ac751c6c97a273306f43d
Mika Senghaas <mail@mikasenghaas.de>
2025-07-05 16:34:08 +0000

    Coding is hard (my type checker would have caught that)

--- src/zeroband/training/orchestrator/orchestrator.py
+++ src/zeroband/training/orchestrator/orchestrator.py
@@ -128,7 +128,7 @@ async def orchestrate(config: OrchestratorConfig, setup_queue: Queue | None = No
 
         # Optionally, wait for the next checkpoint to be available
         async_level = step - 1 - ckpt_step  # How many steps training ahead
-        wait_for_ckpt_time = 0
+        wait_for_ckpt_time, reload_weights_time = 0, 0
         if async_level > config.async_level:
             ckpt_step = step - 1 - config.async_level
             logger.debug(
