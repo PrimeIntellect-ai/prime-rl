Fix normalizing by batch tokens

src/prime_rl/trainer/loss.py
@@ -16,7 +16,7 @@ def grpo_loss(
     original_logprobs: Float[Tensor, "batch seq_minus_1"],
     loss_mask: Int[Tensor, "batch seq"],
     temperature: float,
-    max_tokens: int,
+    total_tokens: int,
     grpo_loss_config: GRPOVariantsConfig,
 ) -> tuple[Tensor, Tensor]:
     if isinstance(grpo_loss_config, ClippingConfig):
@@ -30,7 +30,7 @@ def grpo_loss(
             grpo_loss_config.epsilon_low,
             grpo_loss_config.epsilon_high,
             grpo_loss_config.clip_ratio,
-            max_tokens,
+            total_tokens,
             grpo_loss_config.highest_entropy_ratio_loss,
         )
     elif isinstance(grpo_loss_config, RatioConfig):
@@ -41,7 +41,7 @@ def grpo_loss(
             original_logprobs,
             loss_mask,
             temperature,
-            max_tokens,
+            total_tokens,
             grpo_loss_config.clip_ratio,
             grpo_loss_config.highest_entropy_ratio_loss,
         )
@@ -60,7 +60,7 @@ def grpo_loss_clip(
     epsilon_low: float,
     epsilon_high: float,
     clip_ratio: float,
-    max_tokens: int,
+    total_tokens: int,
     highest_entropy_percentage: float,
 ) -> tuple[Tensor, Tensor]:
     """
@@ -95,12 +95,12 @@ def grpo_loss_clip(
     per_token_loss = torch.max(per_token_loss1, per_token_loss2)
 
     is_clipped = (per_token_loss1 < per_token_loss2).float()
-    clip_ratio_tensor = _apply_mask(is_clipped, loss_mask, max_tokens)
+    clip_ratio_tensor = _apply_mask(is_clipped, loss_mask, total_tokens)
 
     if highest_entropy_percentage < 1.0:
         loss_mask = highest_entropy_mask(logits, loss_mask, highest_entropy_percentage)
 
-    loss = _apply_mask(per_token_loss, loss_mask, max_tokens)
+    loss = _apply_mask(per_token_loss, loss_mask, total_tokens)
 
     return loss, clip_ratio_tensor
 
@@ -114,7 +114,7 @@ def grpo_loss_ratio(
     original_logprobs: Float[Tensor, "batch seq_minus_1"],
     loss_mask: Int[Tensor, "batch seq"],
     temperature: float,
-    max_tokens: int,
+    total_tokens: int,
     clip_ratio: float,
     highest_entropy_percentage: float,
 ) -> tuple[Tensor, Tensor]:
@@ -138,7 +138,7 @@ def grpo_loss_ratio(
     if highest_entropy_percentage < 1.0:
         loss_mask = highest_entropy_mask(logits, loss_mask, highest_entropy_percentage)
 
-    loss = _apply_mask(per_token_loss, loss_mask, max_tokens)
+    loss = _apply_mask(per_token_loss, loss_mask, total_tokens)
 
     ratio_avg = (loss_mask * ratio.detach()).sum() / loss_mask.sum()
 
@@ -204,7 +204,7 @@ def entropy_loss(
     logits: Float[Tensor, "batch seq vocab"],
     loss_mask: Int[Tensor, "batch seq"],
     temperature: float,
-    max_tokens: int,
+    total_tokens: int,
 ) -> Tensor:
     logits = logits[:, :-1, :]
     logits = logits / temperature
@@ -213,11 +213,11 @@ def entropy_loss(
     pd = torch.nn.functional.softmax(logits, dim=-1)
     entropy = torch.logsumexp(logits, dim=-1) - torch.sum(pd * logits, dim=-1)
 
-    return _apply_mask(entropy, loss_mask, max_tokens)
+    return _apply_mask(entropy, loss_mask, total_tokens)
 
 
-def _apply_mask(tensor: Tensor, mask: Tensor, max_tokens: int) -> Tensor:
-    return (tensor * mask).sum() / max_tokens
+def _apply_mask(tensor: Tensor, mask: Tensor, total_tokens: int) -> Tensor:
+    return (tensor * mask).sum() / total_tokens
 
 
 @jaxtyped(typechecker=typechecker)

src/prime_rl/trainer/train.py
@@ -31,7 +31,7 @@ from prime_rl.trainer.utils import (
 from prime_rl.trainer.world import get_world
 from prime_rl.utils.monitor import setup_monitor
 from prime_rl.utils.pydantic_config import parse_argv
-from prime_rl.utils.utils import clean_exit, to_col_format
+from prime_rl.utils.utils import clean_exit, format_num, to_col_format
 
 
 @clean_exit
@@ -206,7 +206,15 @@ def train(config: TrainerConfig):
         forward_backward_start_time = time.time()
         loss_metrics = defaultdict(float)
         num_micro_batches = len(micro_batches)
-        logger.info(f"Starting forward and backward pass ({num_micro_batches=})")
+        micro_batch_size, seq_len = micro_batches[0]["input_ids"].shape
+        batch_size = micro_batch_size * num_micro_batches
+        total_tokens = sum(micro_batch["total_tokens"] for micro_batch in micro_batches)
+        if not config.loss.normalize_to_token_count:
+            total_tokens = batch_size * seq_len
+
+        logger.info(
+            f"Starting forward and backward pass ({num_micro_batches=}, total_tokens={format_num(total_tokens, precision=1)})"
+        )
         for micro_step, micro_batch in enumerate(micro_batches, start=1):
             input_ids = micro_batch["input_ids"].to("cuda")
             position_ids = micro_batch["position_ids"].to("cuda")
@@ -214,13 +222,7 @@ def train(config: TrainerConfig):
             loss_mask = micro_batch["loss_mask"].to("cuda")
             logprobs = micro_batch["logprobs"].to("cuda")
             temperature = micro_batch["temperature"]
-            total_tokens = micro_batch["total_tokens"]
             micro_batch_size, seq_len = input_ids.shape
-
-            # Optionally, normalize the loss to the token count
-            max_tokens = micro_batch_size * seq_len
-            if config.loss.normalize_to_token_count:
-                max_tokens = int(total_tokens)
 
             # Forward pass
             logits = forward(model, input_ids, position_ids).contiguous()
@@ -233,13 +235,13 @@ def train(config: TrainerConfig):
                 logprobs,
                 loss_mask,
                 temperature,
-                max_tokens,
+                total_tokens,
                 config.loss.variant,
             )
 
             # Compute the entropy
             with torch.no_grad():
-                entropy = entropy_loss(logits, loss_mask, temperature, max_tokens)
+                entropy = entropy_loss(logits, loss_mask, temperature, total_tokens)
 
             # Now we can delete the micro batch CUDA tensors
             del micro_batch, logits, input_ids, position_ids, advantages, loss_mask, logprobs