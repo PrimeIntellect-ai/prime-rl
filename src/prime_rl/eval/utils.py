Fix wrong reward computation

src/prime_rl/eval/utils.py
@@ -2,7 +2,6 @@ import asyncio
 import json
 import time
 
-import numpy as np
 import pandas as pd
 from openai import AsyncOpenAI
 
@@ -19,13 +18,9 @@ from prime_rl.utils.monitor import MultiMonitor
 from prime_rl.utils.utils import capitalize
 
 
-def compute_pass_rates(rewards: list[int]):
-    return {f"pass@{k}": compute_pass_at_k(rewards, k) for k in [len(rewards)]}
-
-
-def compute_pass_at_k(rewards: list[int], k: int):
-    sublists = [rewards[i : i + k] for i in range(0, len(rewards), k)]
-    return np.array([any(sublist) for sublist in sublists]).mean()
+def compute_pass_at_k(rewards: list[int]):
+    k = len(rewards)
+    return {f"pass@{k}": any(reward == 1.0 for reward in rewards)}
 
 
 async def run_benchmark(
@@ -85,6 +80,10 @@ async def run_benchmark(
     completions = parse_completions(chat_completions)
     task_types = [item["task_type"] for item in dataset]
     verification_infos = [json.loads(item["verification_info"]) for item in dataset]
+    task_types = [task_type for task_type in task_types for _ in range(rollouts_per_prompt)]
+    verification_infos = [
+        verification_info for verification_info in verification_infos for _ in range(rollouts_per_prompt)
+    ]
     rewards = compute_rewards(completions, task_types, verification_infos)
 
     # Collect rewards
@@ -99,9 +98,9 @@ async def run_benchmark(
     unique_rewards = sample_stats.reward.unique()
     could_be_binary = set(unique_rewards).issubset({0.0, 1.0})
     if could_be_binary:
-        pass_rates = (
+        pass_at_k = (
             sample_stats.groupby("problem_id")
-            .apply(lambda x: compute_pass_rates(x.reward), include_groups=False)
+            .apply(lambda x: compute_pass_at_k(x.reward), include_groups=False)
             .apply(pd.Series)
         )
     else:
@@ -113,14 +112,14 @@ async def run_benchmark(
     benchmark_time = time.time() - benchmark_start_time
     message = f"Evaluated {benchmark_name} in {benchmark_time:.2f}s (Avg@{k}={sample_stats.reward.mean():.2f}"
     if could_be_binary:
-        for pass_rate, pass_rate_score in pass_rates.mean().items():
+        for pass_rate, pass_rate_score in pass_at_k.mean().items():
             message += f", {capitalize(pass_rate)}: {pass_rate_score:.2f}"
     logger.success(message + ")")
 
     # Log statistics to monitor
     eval_metrics = {f"avg@{k}": float(sample_stats.reward.mean())}
     if could_be_binary:
-        eval_metrics.update(pass_rates.mean().to_dict())
+        eval_metrics.update(pass_at_k.mean().to_dict())
     eval_metrics = {**{f"eval/{benchmark}/{k}": v for k, v in eval_metrics.items()}}
     if step is None:
         step = ckpt_step