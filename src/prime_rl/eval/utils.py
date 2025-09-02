062bc76b679aed02d217f77cd86e5b454f703f52
Mika Senghaas <mail@mikasenghaas.de>
2025-09-02 08:34:33 +0000

    Do not compute completion len and truncated ratio for oai models

--- src/prime_rl/eval/utils.py
+++ src/prime_rl/eval/utils.py
@@ -138,16 +138,24 @@ async def run_eval(
     logger.debug(f"Generated and scored rollouts in {run_eval_time:.2f}s")
 
     rewards = torch.tensor(generate_outputs.reward).reshape(-1, rollouts_per_example).float()
-    completion_lens = (
-        torch.tensor(list(map(len, parse_completion_tokens(states=generate_outputs.state))))
-        .reshape(-1, rollouts_per_example)
-        .float()
-    )
-    is_truncated = (
-        torch.tensor(parse_truncated_completions(states=generate_outputs.state))
-        .reshape(-1, rollouts_per_example)
-        .float()
-    )
+    if client_config.server_type == "vllm":
+        completion_lens = (
+            torch.tensor(list(map(len, parse_completion_tokens(states=generate_outputs.state))))
+            .reshape(-1, rollouts_per_example)
+            .float()
+        )
+        is_truncated = (
+            torch.tensor(parse_truncated_completions(states=generate_outputs.state))
+            .reshape(-1, rollouts_per_example)
+            .float()
+        )
+    else:
+        # We can probably do a best-effort tokenization to get an approximate token completion length and truncation detection
+        logger.warning(
+            f"Server type {client_config.server_type} not supported for computing completion length and truncated completions."
+        )
+        completion_lens = torch.zeros(len(examples)).reshape(-1, rollouts_per_example).float()
+        is_truncated = torch.zeros(len(examples)).reshape(-1, rollouts_per_example).float()
 
     k = rollouts_per_example
     sample_stats = pd.DataFrame({"example_id": example_ids, "reward": rewards.flatten().tolist()})
