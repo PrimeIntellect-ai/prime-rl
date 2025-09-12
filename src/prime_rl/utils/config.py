ec2520f1229e6cc6a25cfcb95ce02c03caac273e
Mika Senghaas <mail@mikasenghaas.de>
2025-09-12 11:30:03 +0000

    Do not log extras by default

--- src/prime_rl/utils/config.py
+++ src/prime_rl/utils/config.py
@@ -88,4 +88,4 @@ class WandbMonitorConfig(BaseConfig):
         Field(
             description="Configuration for logging extras to W&B tables. If None, no extras are logged.",
         ),
-    ] = LogExtrasConfig()
+    ] = None
