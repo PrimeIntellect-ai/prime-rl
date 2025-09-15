953d4a1b71300c959924966cc7a92cf1cbc144c6
Mika Senghaas <mail@mikasenghaas.de>
2025-09-15 15:54:58 +0000

    Revert "Do not fail if max_vio not available"
    This reverts commit aae8f447c33af12617190ff9e74ff1cb8eb9b190.

--- src/prime_rl/trainer/sft/train.py
+++ src/prime_rl/trainer/sft/train.py
@@ -179,11 +179,9 @@ def train(config: SFTTrainerConfig):
                 loss = cross_entropy(logits.view(-1, V), target_ids.view(-1), reduction="none").view(B, L)
 
                 if is_tt_moe_model(model):
-                    max_vio = get_load_balance_stats(model).get("max_vio", None) or torch.tensor(0.0).to("cuda")
-                    if max_vio is not None:
-                        max_vio = max_vio.mean()
-                        dist.all_reduce(max_vio, op=dist.ReduceOp.MAX)
-                        batch_max_vio += max_vio / grad_accum_steps
+                    max_vio = get_load_balance_stats(model)["max_vio"].mean()
+                    dist.all_reduce(max_vio, op=dist.ReduceOp.MAX)
+                    batch_max_vio += max_vio / grad_accum_steps
 
                 # Compute average loss over unmasked tokens
                 loss = loss[loss_mask].mean()
