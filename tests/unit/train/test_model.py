0510c41b914eb37948953cc5fc3d688b77f66c29
Sami Jaghouar <sami.jaghouar@hotmail.fr>
2025-09-16 21:18:47 +0000

    skip moe test base on device capability

--- tests/unit/train/test_model.py
+++ tests/unit/train/test_model.py
@@ -105,6 +105,7 @@ def test_model_with_sequence_packing(model, correct_position_ids):
             torch.testing.assert_close(output_packed_right, output_base)
 
 
+@pytest.mark.skipif(torch.cuda.get_device_capability(0)[0] < 9, reason="Requires GPU 0 compute capability >= 9.0")
 def test_moe():
     config = ModelConfig(name="Jackmin108/debug-moe-0.5B", attn="sdpa", trust_remote_code=True)
     model = get_model(config)
