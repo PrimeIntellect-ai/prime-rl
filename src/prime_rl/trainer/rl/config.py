f4e8481eb76cf0e9215e6cadf846337ce1213d04
Jackmin801 <ongjackm@gmail.com>
2025-09-18 23:57:41 -0700

    raise instead of assert

--- src/prime_rl/trainer/rl/config.py
+++ src/prime_rl/trainer/rl/config.py
@@ -171,8 +171,10 @@ class RLTrainerConfig(BaseSettings):
     @model_validator(mode="after")
     def dont_do_massive_traces(self):
         if self.trace_path:
-            assert self.max_steps is not None, "Must specify max_steps when tracing"
-            assert self.max_steps < 10, (
-                "Tracing more than 10 steps is not recommended as your trace will be massive. Remove this line if you really want to trace more steps."
-            )
+            if self.max_steps is None:
+                raise ValueError("Must specify max_steps when tracing")
+            if self.max_steps >= 10:
+                raise ValueError(
+                    "Tracing more than 10 steps is not recommended as your trace will be massive. Remove this line if you really want to trace more steps."
+                )
         return self
