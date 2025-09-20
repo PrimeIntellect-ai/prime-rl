ab5678bcc27fea7a94d2432a70476a1baeab7464
Sami Jaghouar <sami.jaghouar@hotmail.fr>
2025-09-20 01:56:01 +0000

    remve duplicae class

--- src/prime_rl/trainer/custom_models/__init__.py
+++ src/prime_rl/trainer/custom_models/__init__.py
@@ -3,36 +3,19 @@
 from collections import OrderedDict
 
 from transformers import AutoConfig
-from transformers.models.auto.auto_factory import (
-    _BaseAutoModelClass,
-    _get_model_class,
-    _LazyAutoMapping,
-    auto_class_update,
-)
+from transformers.models.auto.auto_factory import _BaseAutoModelClass, _LazyAutoMapping, auto_class_update
 from transformers.models.auto.configuration_auto import CONFIG_MAPPING_NAMES
 from transformers.models.llama.configuration_llama import LlamaConfig
 
-from prime_rl.trainer.custom_models.glm import Glm4MoeConfig, Glm4MoeForCausalLM, Glm4MoeModel
-from prime_rl.trainer.custom_models.llama import LlamaForCausalLM, LlamaModel
+from prime_rl.trainer.custom_models.glm import Glm4MoeConfig, Glm4MoeForCausalLM
+from prime_rl.trainer.custom_models.llama import LlamaForCausalLM
 
 # Make custom config discoverable by AutoConfig
 AutoConfig.register("glm4_moe", Glm4MoeConfig, exist_ok=True)
 
-# Minimal PRIME-RL-only mappings (use OrderedDict, not dict)
-_CUSTOM_BASE_MAPPING = _LazyAutoMapping(CONFIG_MAPPING_NAMES, OrderedDict())
-_CUSTOM_BASE_MAPPING.register(LlamaConfig, LlamaModel, exist_ok=True)
-_CUSTOM_BASE_MAPPING.register(Glm4MoeConfig, Glm4MoeModel, exist_ok=True)
-
 _CUSTOM_CAUSAL_LM_MAPPING = _LazyAutoMapping(CONFIG_MAPPING_NAMES, OrderedDict())
 _CUSTOM_CAUSAL_LM_MAPPING.register(LlamaConfig, LlamaForCausalLM, exist_ok=True)
 _CUSTOM_CAUSAL_LM_MAPPING.register(Glm4MoeConfig, Glm4MoeForCausalLM, exist_ok=True)
-
-
-class AutoModelPrimeRL(_BaseAutoModelClass):
-    _model_mapping = _CUSTOM_BASE_MAPPING
-
-
-AutoModelPrimeRL = auto_class_update(AutoModelPrimeRL)
 
 
 class AutoModelForCausalLMPrimeRL(_BaseAutoModelClass):
@@ -42,17 +25,4 @@ class AutoModelForCausalLMPrimeRL(_BaseAutoModelClass):
 AutoModelForCausalLMPrimeRL = auto_class_update(AutoModelForCausalLMPrimeRL, head_doc="causal language modeling")
 
 
-def get_model_cls(config):
-    return _get_model_class(config, AutoModelForCausalLMPrimeRL._model_mapping)
-
-
-__all__ = [
-    "AutoModelPrimeRL",
-    "AutoModelForCausalLMPrimeRL",
-    "get_model_cls",
-    "LlamaModel",
-    "LlamaForCausalLM",
-    "Glm4MoeConfig",
-    "Glm4MoeModel",
-    "Glm4MoeForCausalLM",
-]
+__all__ = ["AutoModelForCausalLMPrimeRL"]
