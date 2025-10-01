4b9aed19b2682ce779f10a21a210731fe6f05b75
Mika Senghaas <mail@mikasenghaas.de>
2025-10-01 12:21:59 +0000

    Do not uv sync in dev mode

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -10,26 +10,29 @@ log_info() {
     echo -e "${GREEN}[INFO]${NC} $1"
 }
 
-# Turn off dev mode
 if [ "$1" == "off" ]; then
+    # Turn off dev mode
     log_info "Turning off dev mode..."
     uv sync && uv sync --all-extras
+    unset UV_NO_SYNC
     log_info "Turned off dev mode!"
-    exit 0
+else
+    # Turn on dev mode
+    if [ ! -d ~/verifiers ]; then
+        log_info "Setting up verifiers locally..."
+        cd ~ && git clone https://github.com/PrimeIntellect-ai/verifiers.git && cd verifiers && uv sync --extra dev && cd -
+    fi
+
+    if [ ! -d ~/prime-environments ]; then
+        log_info "Setting up prime-environments locally..."
+        cd ~ && curl -sSL https://raw.githubusercontent.com/PrimeIntellect-ai/prime-environments/main/scripts/install.sh | bash && cd -
+    fi
+
+    log_info "Installing verifiers locally..."
+    uv pip install -e ~/verifiers
+    export UV_NO_SYNC=1
+
+    log_info "Turned on dev mode! To turn it off, run \`source scripts/dev.sh off\`"
 fi
 
-# Turn on dev mode
-if [ ! -d ~/verifiers ]; then
-    log_info "Setting up verifiers locally..."
-    cd ~ && git clone https://github.com/PrimeIntellect-ai/verifiers.git && cd verifiers && uv sync --extra dev && cd -
-fi
-
-if [ ! -d ~/prime-environments ]; then
-    log_info "Setting up prime-environments locally..."
-    cd ~ && curl -sSL https://raw.githubusercontent.com/PrimeIntellect-ai/prime-environments/main/scripts/install.sh | bash && cd -
-fi
-
-log_info "Installing verifiers locally..."
-uv pip install -e ~/verifiers
-
-log_info "Turned on dev mode! To turn it off, run \`bash scripts/dev.sh off\`"
+set +e
