eef64135b23e42856d48e739df7e8d78f0936706
Mika Senghaas <mail@mikasenghaas.de>
2025-10-13 03:01:35 -0700

    Update src/prime_rl/trainer/sft/train.py
    Co-authored-by: samsja <55492238+samsja@users.noreply.github.com>
    Signed-off-by: Mika Senghaas <mail@mikasenghaas.de>

--- src/prime_rl/trainer/sft/train.py
+++ src/prime_rl/trainer/sft/train.py
@@ -129,7 +129,6 @@ def train(config: SFTTrainerConfig):
         # This redundant setup is necessary because loading the optimizer's state has side effects on the scheduler state dict
         if config.ckpt.skip_scheduler:
             scheduler = setup_scheduler(optimizer, config.scheduler, scheduler_steps, config.optim.lr)
-        print(scheduler.state_dict())
     logger.info(
         f"Starting from step {progress.step} (total_tokens={progress.total_tokens}, total_samples={progress.total_samples}, dataset_state={dataloader.state_dict()['dataset_state']})"
     )
