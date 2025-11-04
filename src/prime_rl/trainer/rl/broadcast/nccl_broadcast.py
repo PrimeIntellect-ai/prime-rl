fe002e38be1059eace67b564e7c461945a5b8140
Sami Jaghouar <sami.jaghouar@hotmail.fr>
2025-11-04 06:26:53 +0000

    add bucketed send recv

--- src/prime_rl/trainer/rl/broadcast/nccl_broadcast.py
+++ src/prime_rl/trainer/rl/broadcast/nccl_broadcast.py
@@ -7,7 +7,6 @@ from torch.distributed.tensor import DTensor
 from vllm.distributed.device_communicators.pynccl import PyNcclCommunicator
 from vllm.distributed.utils import StatelessProcessGroup
 
-from prime_rl.trainer.rl.broadcast.utils import init_tensor_from_string_description, tensor_string_description
 from prime_rl.trainer.utils import get_world
 from prime_rl.trainer.weights import convert_tt_layer_to_hf, get_max_layer_num, has_tt_moe_layers
 
@@ -23,23 +22,39 @@ def send_state_dict(state_dict: dict[str, torch.Tensor], communicator: PyNcclCom
     """
     Get a state dict of tensor and broadcast it to the other ranks using NCCL.
     """
-    state = pickle.dumps({key: tensor_string_description(value) for key, value in state_dict.items()})
+    # Group tensors by dtype
+    dtype_groups: dict[torch.dtype, list[tuple[str, torch.Tensor]]] = {}
+    for key, value in state_dict.items():
+        assert not isinstance(value, DTensor), (
+            "DTensor is not supported for broadcast, should have been converted to tensor already"
+        )
+        dtype = value.dtype
+        if dtype not in dtype_groups:
+            dtype_groups[dtype] = []
+        dtype_groups[dtype].append((key, value))
+
+    # Build metadata: for each dtype group, store keys and shapes
+    metadata = {}
+    for dtype, items in dtype_groups.items():
+        metadata[dtype] = [(key, value.shape, value.numel()) for key, value in items]
+
+    # Send metadata
+    state = pickle.dumps(metadata)
     size_tensor = torch.tensor([len(state)], dtype=torch.long).cuda()
     communicator.broadcast(size_tensor, src=0)
     state_tensor = torch.ByteTensor(list(state)).cuda()
     communicator.broadcast(state_tensor, src=0)
 
-    # TODO(SAMI): there are two performance optimization we should do here:
-    # 1. we should bucket more tensor into one broadcast call
-    # 2. we should make sure both full_tensor gather that is performed before the broadcast are done in parallel
-
-    for key, value in state_dict.items():
-        assert not isinstance(value, DTensor), (
-            "DTensor is not supported for broadcast, should have been converted to tensor already"
-        )
-        communicator.broadcast(value, src=0)
-
-        del value
+    # Concatenate and broadcast tensors grouped by dtype
+    for dtype, items in dtype_groups.items():
+        # Flatten all tensors and concatenate
+        flat_tensors = [value.flatten() for _, value in items]
+        concatenated = torch.cat(flat_tensors)
+        communicator.broadcast(concatenated, src=0)
+        del concatenated
+        # Clean up individual tensors
+        for _, value in items:
+            del value
 
 
 def receive_state_dict(
@@ -51,15 +66,26 @@ def receive_state_dict(
     state_tensor = torch.empty(size_tensor.item(), dtype=torch.uint8).to(communicator.device)
     communicator.broadcast(state_tensor, src=0)
 
-    state = pickle.loads(bytes(state_tensor.cpu().numpy()))
+    metadata = pickle.loads(bytes(state_tensor.cpu().numpy()))
 
-    for key, value in state.items():
-        tensor = init_tensor_from_string_description(value, communicator.device)
-        communicator.broadcast(tensor, src=0)
-        try:
-            yield key, tensor
-        finally:
-            del tensor
+    # Receive concatenated tensors per dtype and split them back
+    for dtype, tensor_info_list in metadata.items():
+        # Receive concatenated tensor for this dtype
+        total_elements = sum(numel for _, _, numel in tensor_info_list)
+        concatenated = torch.empty(total_elements, dtype=dtype, device=communicator.device)
+        communicator.broadcast(concatenated, src=0)
+
+        # Split concatenated tensor back into individual tensors
+        offset = 0
+        for key, shape, numel in tensor_info_list:
+            tensor = concatenated[offset : offset + numel].view(shape).clone()
+            offset += numel
+            try:
+                yield key, tensor
+            finally:
+                del tensor
+
+        del concatenated
 
 
 def send_integer(integer: int, communicator: PyNcclCommunicator | dist.ProcessGroup) -> None:
@@ -142,7 +168,7 @@ class NCCLBroadcastSender:
             for key, value in list(state_dict.items()):
                 if isinstance(value, DTensor):
                     value = value.to(self.dtype).full_tensor()
-                    
+
                 state_dict[key] = value
 
             if has_tt_moe_layers(state_dict):
