c8be3610bf8497b0ef8b0238f7e762cad2b862d6
Jackmin801 <ongjackm@gmail.com>
2025-11-19 20:30:56 +0000

    add for loop impl

--- src/prime_rl/trainer/models/layers/lora.py
+++ src/prime_rl/trainer/models/layers/lora.py
@@ -2,6 +2,25 @@ import torch
 import torch.nn as nn
 
 
+def _run_lora_grouped_mm(x: torch.Tensor, lora_A: torch.Tensor, lora_B: torch.Tensor, offsets: torch.LongTensor):
+    _a_out = torch._grouped_mm(x, lora_A, offsets)
+    lora_out = torch._grouped_mm(_a_out, lora_B, offsets)
+    return lora_out
+
+
+def _run_lora_for_loop(x: torch.Tensor, lora_A: torch.Tensor, lora_B: torch.Tensor, offsets: torch.LongTensor):
+    lora_out_splits = []
+    for i in range(offsets.shape[0]):
+        if i == 0:
+            _a_out = torch.matmul(x[0 : offsets[i]], lora_A[i])
+            lora_out = torch.matmul(_a_out, lora_B[i])
+        else:
+            _a_out = torch.matmul(x[offsets[i - 1] : offsets[i]], lora_A[i])
+            lora_out = torch.matmul(_a_out, lora_B[i])
+        lora_out_splits.append(lora_out)
+    return torch.cat(lora_out_splits, dim=0)
+
+
 class MultiLoRALinear(nn.Module):
     """
     Linear + multi-LoRA with grouped GEMM (batched matmul).
@@ -27,16 +46,27 @@ class MultiLoRALinear(nn.Module):
         r: int,
         n_adapters: int,
         alpha: float = 16.0,
+        use_grouped_mm: bool = True,
     ):
         super().__init__()
         if r <= 0 or n_adapters <= 0:
             raise ValueError("r and n_adapters must be > 0")
+
+        # Set use_grouped_mm to False if CUDA compute capability < 9.0
+        if torch.cuda.is_available():
+            device = torch.cuda.current_device()
+            cc_major, _ = torch.cuda.get_device_capability(device)
+            if cc_major < 9:
+                use_grouped_mm = False
+        else:
+            use_grouped_mm = False
 
         self.base = base_linear
         self.r = r
         self.n_adapters = n_adapters
         self.alpha = alpha
         self.scaling = alpha / r
+        self.use_grouped_mm = use_grouped_mm
 
         # LoRA weights: one low-rank pair per adapter
         # [n_adapters, in, r]
@@ -74,12 +104,13 @@ class MultiLoRALinear(nn.Module):
         x: [..., in_features]
         offsets: [n_adapters]
         """
-        base_out = self.base(x)
-
-        _a_out = torch._grouped_mm(x, self.lora_A, offsets)
-        lora_out = torch._grouped_mm(_a_out, self.lora_B, offsets)
-
-        return base_out + lora_out
+        assert x.dim() == 2
+        assert offsets[-1] == x.shape[0]
+        if self.use_grouped_mm:
+            lora_out = _run_lora_grouped_mm(x, self.lora_A, self.lora_B, offsets)
+        else:
+            lora_out = _run_lora_for_loop(x, self.lora_A, self.lora_B, offsets)
+        return self.base(x) + lora_out
 
     def __repr__(self):
         return (
