dd4ae0a0b39b3e11997977aefe115ef48bee1f14
Jackmin801 <ongjackm@gmail.com>
2025-11-20 18:20:24 +0000

    Revert "TEMP: update meow.sh to use reverse text model"
    This reverts commit 18326d48838fa2be4839190cd4cf84f45c294cae.

--- meow.sh
+++ meow.sh
@@ -1,3 +1,4 @@
 export VLLM_ALLOW_RUNTIME_LORA_UPDATING=True
 
-uv run inference @ configs/reverse_text/rl/infer.toml --max-model-len 2048 --enable-lora
+vllm serve meta-llama/Llama-2-7b-hf \
+    --enable-lora
