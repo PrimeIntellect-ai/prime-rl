3cbf33b98f21ff4a0fd5b35e8b0b0caeb566136b
Jackmin801 <ongjackm@gmail.com>
2025-11-21 23:27:31 +0000

    TEMP

--- woof1.sh
+++ woof1.sh
@@ -12,12 +12,25 @@
 #   --wandb.project multi-tenant-debug \
 #   --wandb.name r8-1e-4 \
 #   --log.level debug \
-#   --ckpt
 
-uv run orchestrator @ configs/reverse_text/rl/orch.toml \
-  --sampling.max-tokens 128 \
-  --lora-name r8-1e-4 \
-  --output_dir meow_outs \
+
+#uv run orchestrator @ configs/reverse_text/rl/orch.toml \
+#  --sampling.max-tokens 128 \
+#  --lora-name r8-1e-4 \
+#  --output_dir meow_outs \
+#  --wandb.project multi-tenant-debug \
+#  --wandb.name debug \
+#  --log.level debug \
+
+uv run torchrun --nproc-per-node 2 src/prime_rl/trainer/rl/train.py \
+  @ configs/reverse_text/rl/train.toml \
+  --model.experimental.lora.rank 8 \
+  --model.seq_len 8192 \
+  --ckpt.weights.save-adapter-separately \
+  --weight_broadcast.adapter_only \
+  --output_dir multi_out_dev \
+  --optim.lr 1e-4 \
   --wandb.project multi-tenant-debug \
-  --wandb.name debug \
+  --wandb.name r8-1e-4 \
   --log.level debug \
+  --ckpt
