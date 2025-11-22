14f971d2d8e07257fa09af36f2f1609917ebdc90
Jackmin801 <ongjackm@gmail.com>
2025-11-22 05:48:58 +0000

    TEMP

--- woof.sh
+++ woof.sh
@@ -3,12 +3,40 @@ uv run rl \
   --orchestrator @ configs/reverse_text/rl/orch.toml \
   --orchestrator.sampling.max-tokens 128 \
   --trainer.model.experimental.lora.rank 8 \
-  --trainer.weights.save-adapter-separately \
-  --orchestrator.lora-name r8-1e-3 \
+  --trainer.ckpt.weights.save-adapter-separately \
+  --trainer.weight_broadcast.adapter_only \
+  --orchestrator.lora-name r8-1e-4 \
   --trainer_gpu_ids 1 \
-  --output_dir multi_outputs/trainer_1 \
-  --trainer.optim.lr 1e-3 \
+  --output_dir outputs \
+  --trainer.optim.lr 1e-4 \
   --wandb.project multi-tenant-debug \
-  --wandb.name r8-1e-3 \
+  --wandb.name r8-1e-4 \
   --log.level debug \
+  --ckpt.resume-step 20 \
+  --max-steps 25
+
+
+
+#uv run orchestrator @ configs/reverse_text/rl/orch.toml \
+#  --sampling.max-tokens 128 \
+#  --lora-name r8-1e-4 \
+#  --output_dir meow_outs \
+#  --wandb.project multi-tenant-debug \
+#  --wandb.name debug \
+#  --log.level debug \
+
+export CUDA_VISIBLE_DEVICES=1
+uv run torchrun --nproc-per-node 1 src/prime_rl/trainer/rl/train.py \
+  @ configs/reverse_text/rl/train.toml \
+  --model.experimental.lora.rank 8 \
+  --model.seq_len 4096 \
+  --ckpt.weights.save-adapter-separately \
+  --weight_broadcast.adapter_only \
+  --output_dir outputs \
+  --optim.lr 1e-4 \
+  --wandb.project multi-tenant-debug \
+  --wandb.name r8-1e-4-trainer \
+  --log.level debug \
+  --max-steps 100 \
+  --max-async-level 5 \
   --ckpt
