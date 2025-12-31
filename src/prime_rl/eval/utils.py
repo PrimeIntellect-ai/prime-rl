0d21b4c6046c69119feeb7b485f53135816d7a5d
Mika Senghaas <mail@mikasenghaas.de>
2025-12-31 11:36:59 +0530

    force wandb log for now, is_master is true and is_enabled is true. the wandb.log in monitor just noops

--- src/prime_rl/eval/utils.py
+++ src/prime_rl/eval/utils.py
@@ -1,3 +1,4 @@
+import wandb
 import asyncio
 import json
 import re
@@ -537,6 +538,7 @@ async def run_eval(
         eval_metrics.update(pd.Series(pass_at_k.mean()).to_dict())
     eval_metrics = {**{f"eval/{env_name_or_id}/{k}": v for k, v in eval_metrics.items()}}
     eval_metrics.update({"progress/ckpt_step": ckpt_step, "step": step or ckpt_step})
+    wandb.log(eval_metrics)
     monitor.log(eval_metrics)
 
     # Save results
