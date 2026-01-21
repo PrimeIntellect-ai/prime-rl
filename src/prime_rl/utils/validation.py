5dfe1dda98215fae469fa03a84656d2bcc1aea89
kalomaze <66376113+kalomaze@users.noreply.github.com>
2026-01-21 00:02:23 +0000

    [Validate] Assert when only trainer or orchestrator wandb is configured

--- src/prime_rl/utils/validation.py
+++ src/prime_rl/utils/validation.py
@@ -61,6 +61,16 @@ def validate_shared_wandb_config(
     trainer: RLTrainerConfig,
     orchestrator: OrchestratorConfig,
 ) -> None:
+    if trainer.wandb and not orchestrator.wandb:
+        raise ValueError(
+            "Trainer W&B config is specified, but orchestrator W&B config is not. "
+            "This means only trainer metrics will be logged. Please specify [orchestrator.wandb] to log orchestrator metrics as well."
+        )
+    if orchestrator.wandb and not trainer.wandb:
+        raise ValueError(
+            "Orchestrator W&B config is specified, but trainer W&B config is not. "
+            "This means only orchestrator metrics will be logged. Please specify [trainer.wandb] to log trainer metrics as well."
+        )
     if trainer.wandb and orchestrator.wandb:
         if trainer.wandb.project != orchestrator.wandb.project:
             raise ValueError(
