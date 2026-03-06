c9c49fca5803db4e0ff93e1a6ecffa1ceb9674fd
Jannik Straube <info@jannik-straube.de>
2026-03-05 21:19:33 -0800

    align flash-attn-cute rev in scripts with pyproject.toml (e2743ab5)

--- scripts/docker-arm64-post-install.sh
+++ scripts/docker-arm64-post-install.sh
@@ -8,7 +8,7 @@ TORCH_CUDA_ARCH_LIST="10.0" MAX_JOBS=4 \
 
 echo "=== reinstalling flash-attn-cute (flash-attn overwrites it with a stub) ==="
 uv pip install --reinstall --no-deps \
-    "flash-attn-cute @ git+https://github.com/Dao-AILab/flash-attention.git@2b5db43#subdirectory=flash_attn/cute"
+    "flash-attn-cute @ git+https://github.com/Dao-AILab/flash-attention.git@e2743ab5#subdirectory=flash_attn/cute"
 
 # TODO: remove once flash-attn gates the ampere_helpers import or cutlass-dsl re-adds it.
 echo "=== copying ampere_helpers.py from flashinfer vendor ==="
