c571b6d5ac83377df727e40d5249bb59d214cb4f
hallerite <git@hallerite.com>
2026-03-13 03:44:52 +0530

    run ruff

--- src/prime_rl/trainer/rl/broadcast/nccl.py
+++ src/prime_rl/trainer/rl/broadcast/nccl.py
@@ -16,10 +16,10 @@ from prime_rl.trainer.rl.broadcast.base import WeightBroadcast
 from prime_rl.trainer.runs import get_multi_run_manager
 from prime_rl.trainer.utils import get_world
 from prime_rl.trainer.weights import get_max_layer_num
-from prime_rl.utils.vlm import get_layer_prefix
 from prime_rl.utils.logger import get_logger
 from prime_rl.utils.pathing import sync_wait_for_path
 from prime_rl.utils.utils import get_broadcast_dir, get_step_path
+from prime_rl.utils.vlm import get_layer_prefix
 
 NCCL_READY_MARKER = "NCCL_READY"
 
@@ -80,11 +80,7 @@ def filter_state_dict_by_layers(
     for i in range(num_layers):
         yield (
             i,
-            {
-                key: value
-                for key, value in state_dict.items()
-                if key.startswith(f"{layer_prefix}{i}.")
-            },
+            {key: value for key, value in state_dict.items() if key.startswith(f"{layer_prefix}{i}.")},
         )
 
 
