0d0594d082fe9339b889d8e8717562cf7a3fd658
sami jaghouar <sami@primeintellect.ai>
2026-08-14 01:45:54 +0000

    restore state-only optimizer offload

--- src/prime_rl/trainer/utils.py
+++ src/prime_rl/trainer/utils.py
@@ -23,7 +23,7 @@ from prime_rl.utils.pathing import get_ckpt_dir
 from prime_rl.utils.utils import get_step_path
 
 if TYPE_CHECKING:
-    from prime_rl.configs.trainer import OptimizerOffloadingConfig
+    from prime_rl.configs.trainer import FullOptimizerOffloadingConfig
     from prime_rl.trainer.optim import GradientOffloadManager
 
 DEFAULT_TIMEOUT = timedelta(seconds=600)
@@ -164,7 +164,7 @@ def configure_cpu_optimizer_threads() -> None:
     )
 
 
-def setup_cpu_optimizer_offload(config: "OptimizerOffloadingConfig | None") -> None:
+def setup_full_cpu_optimizer_offload(config: "FullOptimizerOffloadingConfig | None") -> None:
     if config is None:
         return
     if config.numa_bind:
