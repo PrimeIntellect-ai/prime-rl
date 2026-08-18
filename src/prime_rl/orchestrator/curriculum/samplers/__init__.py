733c90d7439022728d2666513bc49c8921ba4ded
hallerite <git@hallerite.com>
2026-08-18 18:35:50 +0000

    refactor(orchestrator): align sampler module name

--- src/prime_rl/orchestrator/curriculum/samplers/__init__.py
+++ src/prime_rl/orchestrator/curriculum/samplers/__init__.py
@@ -2,6 +2,6 @@
 
 from prime_rl.orchestrator.curriculum.samplers.base import TaskSampler
 from prime_rl.orchestrator.curriculum.samplers.pool import DifficultyPoolSampler
-from prime_rl.orchestrator.curriculum.samplers.simple import StandardSampler
+from prime_rl.orchestrator.curriculum.samplers.standard import StandardSampler
 
 __all__ = ["DifficultyPoolSampler", "StandardSampler", "TaskSampler"]
